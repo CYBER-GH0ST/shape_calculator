4d73259a6f2f8c1fe451302a018ddbd33e29a91d
Cyber Ghost <64844242+CYBER-GH0ST@users.noreply.github.com>
2023-11-19 21:34:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-# This entrypoint file to be used in development. Start by reading README.md
+# app.py
+from flask import Flask, render_template
 import shape_calculator
 from unittest import main
 
+app = Flask(__name__, template_folder='.')
 
-rect = shape_calculator.Rectangle(5, 10)
-print(rect.get_area())
-rect.set_width(3)
-print(rect.get_perimeter())
-print(rect)
+@app.route('/')
+def index():
+    rect = shape_calculator.Rectangle(5, 10)
+    rect_area = rect.get_area()
+    rect.set_width(3)
+    rect_perimeter = rect.get_perimeter()
 
-sq = shape_calculator.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
+    sq = shape_calculator.Square(9)
+    sq_area = sq.get_area()
+    sq.set_side(4)
+    sq_diagonal = sq.get_diagonal()
 
+    return render_template('index.html', rect_area=rect_area, rect_perimeter=rect_perimeter,
+                           sq_area=sq_area, sq_diagonal=sq_diagonal)
 
-# Run unit tests automatically
-main(module='test_module', exit=False)
+if __name__ == '__main__':
+    app.run(debug=True)
